352b31443f535d784d6e01dbb1c672c22ba56d40
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2024-06-28 01:02:21 +0800

    Changed t-SNE display

--- tllib/utils/analysis/tsne.py
+++ tllib/utils/analysis/tsne.py
@@ -30,18 +30,25 @@ def visualize(source_feature: torch.Tensor, target_feature: torch.Tensor,
     features = np.concatenate([source_feature, target_feature], axis=0)
 
     # map features to 2-d using TSNE
-    X_tsne = TSNE(n_components=2, random_state=33).fit_transform(features)
-
-    # domain labels, 1 represents source while 0 represents target
-    domains = np.concatenate((np.ones(len(source_feature)), np.zeros(len(target_feature))))
+    X_tsne = TSNE(n_components=2, random_state=0).fit_transform(features)
 
     # visualize using matplotlib
-    fig, ax = plt.subplots(figsize=(10, 10))
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=domains, cmap=col.ListedColormap([target_color, source_color]), s=20)
-    plt.xticks([])
-    plt.yticks([])
+    plt.figure(figsize=(10, 5))
+    plt.plot(X_tsne[:len(source_feature), 0], X_tsne[:len(source_feature), 1], '.', label="source")
+    plt.plot(X_tsne[len(source_feature):, 0], X_tsne[len(source_feature):, 1], '.', label="target")
+    plt.legend(fontsize=12)
+    plt.title("t-SNE Visualization of Encoded Space")
+
+    # domain labels, 1 represents source while 0 represents target
+    # domains = np.concatenate((np.ones(len(source_feature)), np.zeros(len(target_feature))))
+    
+    # visualize using matplotlib
+    # fig, ax = plt.subplots(figsize=(10, 10))
+    # ax.spines['top'].set_visible(False)
+    # ax.spines['right'].set_visible(False)
+    # ax.spines['bottom'].set_visible(False)
+    # ax.spines['left'].set_visible(False)
+    # plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=domains, cmap=col.ListedColormap([target_color, source_color]), s=20)
+    # plt.xticks([])
+    # plt.yticks([])
     plt.savefig(filename)
