3fdedd0dadfd402ce49c7de10640a89b31e7c004
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2024-07-01 01:32:54 +0800

    Update scripts

--- src/test.sh
+++ src/test.sh
@@ -1,11 +1,3 @@
-# $log_path = 'DomainNetqtor'
-# $data_folder = 'domainnet'
-# $dataset_name = 'DomainNet'
-# $source = q
-# $target = r
-
-
 # CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/adda.py data/$data_folder -d $dataset_name -s $source -t $target --arch resnet50 --pretrain logs/adda/$log_path/checkpoints/best.pth --phase test --seed 1 --log logs/adda/$log_path
 # CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/adda.py data/optical-isar -d Optical_ISAR -s o -t i --arch resnet50 --pretrain logs/adda/optical-isar/checkpoints/best.pth --phase test --seed 1 --log logs/adda/optical-isar
-
-CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/mdd.py data/optical-isar -d Optical_ISAR -s o -t i --arch resnet50 --epochs 5 --pretrain logs/mdd/optical-isar/checkpoints/latest.pth --phase test --seed 1 --log logs/mdd/optical-isar
+CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/mdd.py data/optical-isar -d Optical_ISAR -s o -t i --arch resnet50 --epochs 5 --pretrain logs/mdd/optical-isar/checkpoints/best.pth --phase test --seed 1 --log logs/mdd/optical-isar
--- src/train.sh
+++ src/train.sh
@@ -1,9 +1,3 @@
-# $log_path = 'optical-isar'
-# $data_folder = 'optical-isar'
-# $dataset_name = 'Optical_ISAR'
-# $source = o
-# $target = i
-
 # CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/adda.py data/$data_folder -d $dataset_name -s $source -t $target --arch resnet50 --epochs 10 --seed 1 --log logs/adda/$log_path
 # CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/adda.py data/optical-isar -d Optical_ISAR -s o -t i --arch resnet50 --epochs 5 --seed 1 --log logs/adda/optical-isar
-CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/mdd.py data/optical-isar -d Optical_ISAR -s o -t i --arch resnet50 --epochs 10 --batch-size 32 --iters-per-epoch 100 --print-freq 10 --log logs/mdd/optical-isar --pretrain logs/mdd/optical-isar/checkpoints/latest.pth
+CUDA_VISIBLE_DEVICES=0 python examples/domain_adaptation/image_classification/mdd.py data/optical-isar -d Optical_ISAR -s o -t i --arch resnet50 --epochs 10 --batch-size 32 --iters-per-epoch 100 --print-freq 10 --log logs/mdd/optical-isar --pretrain logs/mdd/optical-isar/checkpoints/best.pth
